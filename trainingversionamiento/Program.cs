7606e799f145db049a19885e87b27787e735254a
elmerreal98 <74421497+elmerreal98@users.noreply.github.com>
2021-09-25 19:22:13 -0600

    Se agrega la solucion del proyecto
    Cree el proyecto de c# y copie los archivos para comenzar a versionarlo.

---
+++ trainingversionamiento/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace trainingversionamiento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Numero 1:");
+
+            // Create a string variable and get user input from the keyboard and store it in the variable
+            string number1 = Console.ReadLine();
+            Console.WriteLine("Numero 2:");
+            // Create a string variable and get user input from the keyboard and store it in the variable
+            string number2 = Console.ReadLine();
+
+            int resultado = Int32.Parse(number1) + Int32.Parse(number2);
+            Console.WriteLine($"{number1} + {number2} = {resultado.ToString()}");
+
+        }
+    }
+}
